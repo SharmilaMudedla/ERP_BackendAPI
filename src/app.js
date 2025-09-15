ce2003c7691b6d3caae1904dbbc0c50c2a8743dc
Sharmila Mudedla <sharmila@makersmind.in>
2025-09-15 14:27:45 +0530

    Cadded the cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 import userRouter from "./Routes/user.routes.js";
 import roleRouter from "./Routes/role.routes.js";
 import fileRouter from "./Routes/file.routes.js";
 const app = express();
-
 app.use(express.json());
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("uploads"));
 app.use("/api/uploads", fileRouter);
