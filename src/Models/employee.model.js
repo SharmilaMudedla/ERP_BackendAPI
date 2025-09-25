6dc37b3d5cbaeb67623921e4baaeafa4b84e9178
BhavaniKamadi <bhavani@spondias.com>
2025-09-25 15:42:39 +0530

    changes

--- src/Models/employee.model.js
+++ src/Models/employee.model.js
@@ -27,10 +27,12 @@ const employeeSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     phone: {
       type: String,
       required: true,
+      unique: true,
     },
     image: {
       type: String,
